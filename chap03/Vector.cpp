4859659b6dd890878898c51fceb0e50a1990a89a
Joker <datuan1611@gmail.com>
2019-12-04 17:08:36 +0700

    Modified Vector.cpp

--- chap03/Vector.cpp
+++ chap03/Vector.cpp
@@ -3,12 +3,12 @@
 #include<Vector.h>		//get  the interface
 
 Vector::Vector(int s)	//intialize members
-//	:elem{new double[s]}, sz{s}
+	:elem{new double[s]}, sz{s}
 {
 	if(s<0)
 		throw length_error{};
-	elem = new double[s];
-	sz = s;
+	//elem = new double[s];
+	//sz = s;
 }
 
 double& Vector::operator[](int i)
